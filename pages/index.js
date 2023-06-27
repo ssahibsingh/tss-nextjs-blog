d4423869b1d6f0303921a863f16f3a2b9fae030b
ssahibsingh <snone181@gmail.com>
2023-06-27 11:33:46 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import { useEffect, useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
-import axios from "axios";
 import blogs from "../data/blogs.js";
 
 const Home = () => {
